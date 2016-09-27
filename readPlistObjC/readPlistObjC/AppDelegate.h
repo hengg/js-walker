30b2c64a8fd91323dfd67c7dfd3a73073965d938
GK-N <wangliang4app@163.com>
2016-09-27 11:16:29 +0800

    read plist file with OC

---
+++ readPlistObjC/readPlistObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  readPlistObjC
+//
+//  Created by GK-N-WANGLIANG on 16/9/27.
+//  Copyright © 2016年 hengg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
