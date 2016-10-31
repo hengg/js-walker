6963711338e4a3080885fe730ef80dd964805532
Liang Wang <wangliang826@hotmail.com>
2016-10-31 17:26:20 +0800

    Update Sample.js

--- ReactNative/SampleNavigation/Sample.js
+++ ReactNative/SampleNavigation/Sample.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+//index.ios.js
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -46,3 +46,28 @@ export default class SampleApp extends Component {
 }
 
 AppRegistry.registerComponent('SampleApp', () => SampleApp);
+
+//MyScene.js
+import React, { Component, PropTypes } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+
+export default class MyScene extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    onForward: PropTypes.func.isRequired,
+    onBack: PropTypes.func.isRequired,
+  }
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: { this.props.title }</Text>
+        <TouchableHighlight onPress={this.props.onForward}>
+          <Text>点我进入下一场景</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.props.onBack}>
+          <Text>点我返回上一场景</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
