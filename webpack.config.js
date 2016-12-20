b99f243425247b4b449d6e832f54fd36afcdb910
GK-N <wangliang4app@163.com>
2016-12-20 14:58:11 +0800

    add deploy config

--- webpack.config.js
+++ webpack.config.js
@@ -31,4 +31,25 @@ var config = {
     }
 };
 
-module.exports = config;
+//deploy config
+// module.exports = config;
+//
+// var path = require('path');
+// var node_modules_dir = path.resolve(__dirname, 'node_modules');
+//
+// var config = {
+//   entry: path.resolve(__dirname, 'app/main.js'),
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [{
+//       test: /\.js$/,
+//       exclude: [node_modules_dir],
+//       loader: 'babel'
+//     }]
+//   }
+// };
+//
+// module.exports = config;
