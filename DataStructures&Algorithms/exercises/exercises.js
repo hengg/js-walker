3ab139da2b969486840df8af2bc0ba5f4dd586ee
hengg <wangliang826@hotmail.com>
2018-09-27 20:02:29 +0800

    :tada: solution

--- DataStructures&Algorithms/exercises/exercises.js
+++ DataStructures&Algorithms/exercises/exercises.js
@@ -1,99 +1,65 @@
-/**
- * 实现如下函数
- * accum("abcd"); // "A-Bb-Ccc-Dddd"
- * accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
- * accum("cwAt"); // "C-Ww-Aaa-Tttt"
+/*观察下面的规律， 写一个函数accum
+accum("abcd"); // "A-Bb-Ccc-Dddd"
+accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt"); // "C-Ww-Aaa-Tttt"
 */
+const repeat = (string, n) => {
+    let upCase = string.toUpperCase()
+    return upCase.concat(new Array(n + 1).join(string.toLowerCase()))
+}
 const accum = (string) => {
+    // const array = string.split('')
+    // let resluts = []
+    // for (let i = 0; i < array.length; i++) {
+    //     resluts.push(repeat(array[i], i))
+    // }
+    // return resluts.reduce((acc, item) => { return acc.concat(item) }, []).join('-')
     const array = Array.from(string);
     return array.reduce((acc, item, index) => {
-        let str = item.toUpperCase().padEnd(index + 1, char.toLowerCase());
-        return acc.concat(str);
-    }, []).join('-');
+        let str = item.toUpperCase().padEnd(index + 1, item.toLowerCase())
+        return acc.concat(str)
+    }, []).join('-')
 }
-// const repeat = (string, n) => {
-//     let upCase = string.toUpperCase()
-//     return upCase.concat(new Array(n + 1).join(string.toLowerCase()))
-// }
-// const accum = (string) => {
-//     let resluts = []
-//     for (let i = 0; i < array.length; i++) {
-//         resluts.push(repeat(array[i], i))
-//     }
-//     return resluts.reduce((acc, item) => { return acc.concat(item) }, []).join('-')
-// }
-module.exports = accum;
-
-/**
- * 实现如下函数,获取数字字符串的最大值和最小值
- * highAndLow("1 2 3 4 5"); // "5 1"
- * highAndLow("1 2 -3 4 5"); // "5 -3"
- * highAndLow("1 9 3 4 -5"); // "9 -5"
- */
-// const highAndLow = (string) => {
-//     const array = string.split(' ')
-//     for (let i = 0; i < array.length; i++) {
-//         let minId = i;
-//         for (let j = i + 1; j < array.length; j++) {
-//             if (array[j] < array[minId]) {
-//                 minId = j
-//             }
-//         }
-//         [array[i], array[minId]] = [array[minId], array[i]]
-//     }
-//     return `${array[array.length - 1]} ${array[0]}`
-// }
+// highAndLow("1 2 3 4 5"); // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
 const highAndLow = (string) => {
-    const array = string.split(' ');
-    array.sort((a, b) => a - b);
-    return `${array[arr.length - 1]} ${array[0]}`;
+    const array = string.split(' ')
+    for (let i = 0; i < array.length; i++) {
+        let minId = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[minId]) {
+                minId = j
+            }
+        }
+        [array[i], array[minId]] = [array[minId], array[i]]
+    }
+    return `${array[array.length - 1]} ${array[0]}`
 }
-module.exports = highAndLow;
-
-/**
- * 实现如下函数,判断字符串内X与O的数量是否相等,忽略大小写
- * XO("ooxx") => true
- * XO("xooxx") => false
- * XO("ooxXm") => true
- * XO("zpzpzpp") => true
- */
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // 没有x也没有o，所有相等，都为0
+// XO("zzoo") => false
 const XO = (string) => {
-    let array = Array.from(string);
-    let x = array.filter(char => char.toUpperCase() === 'X').length;
-    let o = array.filter(char => char.toUpperCase() === 'O').length;
-    return x === o;
+    let array = Array.from(string)
+    let x = array.filter(char => char.toUpperCase() === 'X').length
+    let o = array.filter(char => char.toUpperCase() === 'O').length
+    return x === o
 }
-module.exports = XO;
-/**
- * 判断一个数字是不是某个整数的平方
- * isSquare(2); // false 
- * isSquare(4); // true 
- */
+// 写一个函数判断一个数字是不是某个整数的平方
 const isSquare = (number) => {
-    return Math.sqrt(number) === Math.floor(Math.sqrt(number));
+    return Math.sqrt(number) === Math.floor(Math.sqrt(number))
 }
-module.exports = isSquare
-/**
- * 将字符串除了最后的四位，其他都变成#
- * maskify("4556364607935616") == "############5616"
- * maskify("64607935616") == "#######5616"
- * maskify("1") == "1"
- * maskify("") == ""
- */
-// const maskify = (string) => {
-//     return string.length > 3 ?
-//         Array.from(string).reduce((acc, item, index) => {
-//             return index < string.length - 4 ? acc + '#' : acc + item
-//         }, '')
-//         : string
-// }
-const maskify = (string) => {
-    if ((typeof string) !== 'string') throw new error("argument is not string");
-    // return string.length > 4 ? string.slice(-4).padStart(string.length, '#') : string
-    return string.length > 4 ? string.slice(0, -4).replace(/./g, '#') + string.slice(-4) : string
-}
-module.exports = maskify
 
+const maskify = (string) => {
+    // return string.length > 3 ?
+    //     Array.from(string).reduce((acc, item, index) => {
+    //         return index < string.length - 4 ? acc + '#' : acc + item
+    //     }, '')
+    //     : string
+    return string.length > 4 ? string.slice(-4).padStart(string.length, '#') : string
+}
 /**
  * 三角数列求第N列相加之和
  * 1
@@ -106,4 +72,48 @@ const sumOfRow = (n) => {
     if ((typeof n) !== 'number' || n <= 0) throw 'argument error';
     return Math.pow(n, 3);
 }
-module.exports = sumOfRow;
+/**
+ * 将数字的每一位求平方，然后组合成新的数字
+ */
+const squareDigits = (number) => {
+    if ((typeof number) !== 'number' || number < 0) throw 'argument error';
+    return parseInt(Array.from(number + '').reduce((acc, item) => acc + Math.pow(parseInt(item), 2), ''));
+}
+/**
+ * 求比一个数字n小的所有3和5的整数倍数和,如果输入负数，返回0
+ * solution(10):比10小的3,5整数倍数有:3,5,6,9,所以和为23
+ * solution(16):比16小的3,5整数倍数有:3,5,6,9,10,12,15,所以和为60,其中15只参与一次计算
+ */
+const solution = (number) => {
+    if ((typeof number) !== 'number') throw 'argument error';
+    if (number <= 0) {
+        return 0
+    } else {
+        return Array(number).fill('').reduce((acc, v, i) => acc + (i % 3 === 0 || i % 5 === 0 ? i : 0), 0)
+    }
+}
+/**
+ * A是一个已排序的数组，x是目标值。
+ * 如果找到目标值，返回目标值在数组中的序号。
+ * 如果没有找到目标值，返回目标值应该被插入的位置
+ */
+const bsearch = (array, item) => {
+    // TODO:
+    let low = 0
+    let high = array.length - 1
+    while (low <= high) {
+        let mid = parseInt((low + high) / 2)// JS除法不取整
+        if (item === array[mid]) {
+            return mid
+        }
+        if (item < array[mid]) {
+            high = mid - 1
+        }
+        else {
+            low = mid + 1
+        }
+    }
+    return -1
+}
+
+module.exports = { squareDigits, sumOfRow, accum, maskify, XO, highAndLow, isSquare, solution, bsearch }
--- DataStructures&Algorithms/exercises/exercises.test.js
+++ DataStructures&Algorithms/exercises/exercises.test.js
@@ -1,81 +1,90 @@
-const accum = require('../exercises.js')
+const { accum } = require('./exercises.js')
 test('return regular string', () => {
     expect(accum('abcd')).toBe('A-Bb-Ccc-Dddd')
 })
-
 test('return regular string', () => {
     expect(accum('RqaEzty')).toBe('R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy')
 })
-
 test('return regular string', () => {
     expect(accum('cwAt')).toBe('C-Ww-Aaa-Tttt')
 })
 
-const highAndLow = require('./exercises.js')
+const { highAndLow } = require('./exercises.js')
 test('return regular string', () => {
     expect(highAndLow("1 2 3 4 5")).toBe('5 1')
 })
-
 test('return regular string', () => {
     expect(highAndLow("1 2 -3 4 5")).toBe('5 -3')
 })
-
 test('return regular string', () => {
     expect(highAndLow("1 9 3 4 -5")).toBe('9 -5')
 })
 
-const XO = require('./exercises.js')
+const { XO } = require('./exercises.js')
 test('return regular boolean', () => {
     expect(XO("ooXx")).toBe(true)
 })
-
 test('return regular boolean', () => {
     expect(XO("zzoo")).toBe(false)
 })
-
 test('return regular boolean', () => {
-    expect(XO("zpzpzpp")).toBe(false)
+    expect(XO("zpzpzpp")).toBe(true)
 })
 
-const isSquare = require('./exercises.js')
+const { isSquare } = require('./exercises.js')
 test('return regular boolean', () => {
     expect(isSquare(0)).toBe(true)
 })
-
 test('return regular boolean', () => {
     expect(isSquare(1)).toBe(true)
 })
-
 test('return regular boolean', () => {
     expect(isSquare(3)).toBe(false)
 })
-
 test('return regular boolean', () => {
     expect(isSquare(-1)).toBe(false)
 })
 
-const maskify = require('./exercises.js')
+const { maskify } = require('./exercises.js')
 test('return regular string', () => {
     expect(maskify('4556364607935616')).toBe('############5616')
 })
-
 test('return regular string', () => {
-    expect(maskify('1')).toBe('1')
+    expect(maskify('1111')).toBe('1111')
 })
-
 test('return regular string', () => {
     expect(maskify('')).toBe('')
 })
 
-const sumOfRow = require('./exercises.js')
+const { sumOfRow } = require('./exercises.js')
 test('return regular number', () => {
     expect(sumOfRow(2)).toBe(8)
 })
-
 test('return regular number', () => {
     expect(sumOfRow(3)).toBe(27)
 })
-
 test('return regular number', () => {
     expect(sumOfRow(42)).toBe(74088)
+})
+
+const { squareDigits } = require('./exercises.js')
+test('return regular number', () => {
+    expect(squareDigits(2)).toBe(4)
+})
+test('return regular number', () => {
+    expect(squareDigits(12)).toBe(14)
+})
+test('return regular number', () => {
+    expect(squareDigits(9119)).toBe(811181)
+})
+
+const { solution } = require('./exercises.js')
+test('solution return regular number', () => {
+    expect(solution(10)).toBe(23)
+})
+test('solution return regular number', () => {
+    expect(solution(16)).toBe(60)
+})
+test('solution return regular number', () => {
+    expect(solution(1)).toBe(0)
 })
